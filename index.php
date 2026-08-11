e9685e2da713422e66e67f6723227f7e07a8be46
DESKTOP-1A5LLDF\Owner <ikagawa.office@gmail.com>
2026-08-11 10:16:05 +0900

    firstcomit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>自作サイトテスト</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <h1><?php echo "Hello World! PHPが動いています。"; ?></h1>
+</body>
+
+</html>
