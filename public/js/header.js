5cefa6272abeadc6ff987783c38cf31064174321
seiya <ikagawa.office@gmail.com>
2026-08-14 07:52:22 +0900

    ハンバーガーメニューを展開するボタンが、メニューアイコンと×で切り替わる仕様に変更

--- public/js/header.js
+++ public/js/header.js
@@ -1,20 +1,31 @@
 /**
- *  ヘッダーのJavaScript
+ *  ヘッダーのjs
+ */
+
+/**
+ * ハンバーガーメニューのjs
  */
 document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.getElementById('js-menu-btn');
   const navContent = document.getElementById('js-nav-content');
-  const body = document.body;
+  const closeBtn = document.getElementById('js-close-btn');
 
   if (menuBtn && navContent) {
     menuBtn.addEventListener('click', () => {
       menuBtn.classList.toggle('is-active');
       navContent.classList.toggle('is-open');
-      body.classList.toggle("is-nav-open");
+    });
+  }
+
+  if (menuBtn && navContent && closeBtn) {
+    closeBtn.addEventListener('click', () => {
+      menuBtn.classList.toggle('is-active');
+      navContent.classList.toggle('is-open');
     });
   }
 });
 
+/**ハンバーガーメニュー内にある、ドロップダウンのjs */
 document.addEventListener('DOMContentLoaded', () => {
   const dropdownBtn = document.getElementById('js-dropdown-btn');
   const dropdownMenu = document.getElementById('js-dropdown-menu');
