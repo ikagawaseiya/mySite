7a94ea2616620fd58ea1279d385a742e8df3dcaa
FLORA222-PC\flora222 <flora.s.ikagawa@gmail.com>
2026-08-24 14:55:17 +0900

    いいねした人の識別処理をcookieに変更する準備

--- src/Component/likeButton/likeButtonController.php
+++ src/Component/likeButton/likeButtonController.php
@@ -22,6 +22,19 @@ function showLikeButton(): void
 
   $ipAddress = $_SERVER['REMOTE_ADDR'] ?? null;
 
+  //クッキーを取得する
+  //期限は172800秒（二日とする）
+  //TODO 後に使う
+  $likeUserCookie = "like_user_cookie";
+  if (isset($_COOKIE[$likeUserCookie])) {
+    $currentUserCookie = $_COOKIE[$likeUserCookie];
+  } else {
+    $currentUserCookie = bin2hex(random_bytes(16));
+    $numberOfSecondsInTwoDays = 172800;
+    $expire_time = time() + $numberOfSecondsInTwoDays;
+    setcookie($likeUserCookie, $currentUserCookie, $expire_time, "/", "", true, true);
+  }
+
   require_once __DIR__ . '/likeButtonView.php';
 }
 
